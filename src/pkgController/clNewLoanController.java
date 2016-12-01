a1da886668045fc8e4932693244b80f10b7ae680
DavSosMie <DavSosMie>
2016-12-01 13:04:14 +0000

    dialogo new loan acabado empezando controlador

---
+++ src/pkgController/clNewLoanController.java
@@ -0,0 +1,17 @@
+package pkgController;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class clNewLoanController implements ActionListener {
+
+    public clNewLoanController() {
+        
+        
+    }  
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+    }
+}
